8c61bb414ff1f393be8eff060175959398a6af39
Guilherme Pagani <guilherme.pagani449@al.unieduk.com.br>
2025-06-04 16:00:59 -0300

    feat: adiciona quizzes ENFASE e LABORATORIO com base em arquivos txt convertidos

--- src/app/utils/getQuizData.ts
+++ src/app/utils/getQuizData.ts
@@ -1,7 +1,13 @@
 import SDC from '../data/SDC.json';
+import SFC from '../data/SFC.json';
+import ENFASE from '../data/ENFASE.json';
+import LABORATORIO from '../data/LABORATORIO.json';
 
 const quizzes = [
-  { title: 'SDC', data: SDC }
+  { title: 'SDC', data: SDC },
+  { title: 'SFC', data: SFC },
+  { title: 'ENFASE', data: ENFASE },
+  { title: 'LABORATORIO', data: LABORATORIO },
 ];
 
 export function getQuizByTitle(title: string) {
@@ -10,4 +16,3 @@ export function getQuizByTitle(title: string) {
     (q) => q.title && q.title.toLowerCase() === title.toLowerCase()
   );
 }
-
